1d11de55cae28e40927705ce21d56cdd6ce9d173
Björn Larsson <registrering+github@bjornlarsson.se>
2018-03-30 23:26:35 +0200

    Extracted code to separate methods.

--- ReadMagicCard/OcvCardReaderApplication.cpp
+++ ReadMagicCard/OcvCardReaderApplication.cpp
@@ -1,6 +1,5 @@
 #include "stdafx.h"
 #include "OcvCardReaderApplication.h"
-#include "OcvCardCollectionReader.h"
 #include "OcvFileHandling.h"
 #include "OcvWindows.h"
 #include <iostream>
@@ -19,90 +18,140 @@ OcvCardReaderApplication::~OcvCardReaderApplication()
 int OcvCardReaderApplication::Run(const bool doDebugging, const bool runParallelized) {
 
 	//Store the start time for later check of efficiency.
-	auto start_time = chrono::high_resolution_clock::now();
+	auto startTime = chrono::high_resolution_clock::now();
 
 	//Print a welcome message!
 	wcout << L"Welcome to the eminent MtG card reader! Let's read some cards! :-D" << endl << endl;
 
-	//Create variable telling if the run was successful
-	bool successful = true;
-
 	//Create an object handling the system dependent methods.
-	OcvSystemDependencyClass *systemMethodsPointer = new OcvWindows();
+	OcvSystemDependencyClass* systemMethods = new OcvWindows();
 
 	//Remove old data.
-	wstring folderPath = OcvFileHandling::GetSubFolderPath(systemMethodsPointer, L"Image Data");
-	boost::filesystem::remove_all(folderPath);
+	removeOldData(systemMethods);
 
 	//Fetch all the file names of the card images.
-	wstring mtgFolderPath = OcvFileHandling::GetMtgImageFileFolderPath(systemMethodsPointer);
-	wcout << L"Looking for card images in folders . . ." << endl;
-	wcout << mtgFolderPath << endl << endl;
-	vector<wstring> filenamesOfImages = OcvFileHandling::GetMtgImageFileNames(mtgFolderPath);
+	vector<wstring> filenamesOfImages = getMtgImageFileNames(systemMethods);
 
 	//Check if there are to many files to handle.
 	size_t numberOfFiles = filenamesOfImages.size();
 	if (numberOfFiles <= (size_t)OcvCardCollectionReader::MaxSize()) {
 
-		//Create a card collection reader.
-		OcvCardCollectionReader readers(systemMethodsPointer, doDebugging, runParallelized);
-
-		//Run the reader for every card in the working folder.
-		for (size_t i = 0; i < numberOfFiles; i++) {
-
-			//Get the current element.
-			wstring filename = filenamesOfImages[i];
-
-			//Add the cards to the reader collection
-			readers.AddCard(filename);
-		}
-
+		//Create a reader for every card.
+		OcvCardCollectionReader* readers = createCardReaderCollection(systemMethods, filenamesOfImages, doDebugging, runParallelized);
+		
 		//Resize console window to avoid line breaks.
-		int lettersToAccommodate = to_string(numberOfFiles).length() * 2 + readers.LongestFilenameLength() + 75;
-		systemMethodsPointer->SetConsoleWidthInCharacters(lettersToAccommodate);
-
+		reziseCommandWindow(systemMethods, numberOfFiles, readers->LongestFilenameLength());
+		
 		//Fetch the card names.
-		auto result = readers.ExtractCardNames();
-
-		//Check if there was any errors.
-		for (auto info : result) {
-			if (!info.Success) {
-				successful = false;
-				break;
-			}
-		}
+		auto result = readers->ExtractCardNames();
 
 		//Give a reassuring message... or not.
-		wstring message;
-		if (successful) {
-			message = L"All images was successfully read! Yeay! :-)";
-		}
-		else {
-			int amountOfErrorsInt = readers.AmountOfErrors();
-			wstring amountOfErrorsStr = to_wstring(amountOfErrorsInt);
-			wstring errorWord = (amountOfErrorsInt == 1) ? L"error" : L"errors";
-			message = L"Oh no! There was " + amountOfErrorsStr + L" " + errorWord + L" when reading the cards! :-(";
-		}
-		wcout << endl << message << endl;
+		bool successful = checkIfExecutionWasSuccessful(result);
+		printResultMessage(successful, readers->AmountOfErrors());
+
+		//Since we don't need the readers anymore...
+		delete readers;
 	}
 	else {
 
 		//Tell the user that there was to many files for the reader to handle.
-		wcout << endl << L"Glunck! There were to many cards for me to handle! Don't feed me more than " + to_wstring(OcvCardCollectionReader::MaxSize()) + L" card image files!" << endl;
+		printToManyFilesMessage();
 	}
 
+	//Go memory! Be FREEEE!!
+	delete systemMethods;
+	
 	//Print out how long the program took to execute.
-	auto end_time = chrono::high_resolution_clock::now();
-	auto executionDurationTime = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count();
-	wstring exeTime = doDebugging ? to_wstring(executionDurationTime) : to_wstring(executionDurationTime / (float)1000000);
-	wstring timeUnit = doDebugging ? L"microseconds" : L"seconds";
-	wcout << endl << L"The reading took " + exeTime + L" " + timeUnit + L" to execute." << endl;
-	wcout << L"That's " + to_wstring(executionDurationTime / (numberOfFiles * (float)1000000)) + L" seconds per card on average!" << endl << endl;
-
-	//Destroy the pointer to the object handling the system dependent methods.
-	delete systemMethodsPointer;
+	TimePoint endTime = chrono::high_resolution_clock::now();
+	printExecutionTimeMessage(startTime, endTime, numberOfFiles, !doDebugging);
 
 	//Wait for a keystroke in the window.
 	system("pause");
 	return 0;
+}
+
+void OcvCardReaderApplication::removeOldData(OcvSystemDependencyClass* systemMethods) {
+
+	wstring folderPath = OcvFileHandling::GetSubFolderPath(systemMethods, L"Image Data");
+	boost::filesystem::remove_all(folderPath);
+}
+
+vector<wstring> OcvCardReaderApplication::getMtgImageFileNames(OcvSystemDependencyClass* systemMethods) {
+
+	wstring mtgFolderPath = OcvFileHandling::GetMtgImageFileFolderPath(systemMethods);
+	wcout << L"Looking for card images in folders . . ." << endl;
+	wcout << mtgFolderPath << endl << endl;
+	vector<wstring> filenamesOfImages = OcvFileHandling::GetMtgImageFileNames(mtgFolderPath);
+
+	return filenamesOfImages;
+}
+
+OcvCardCollectionReader* OcvCardReaderApplication::createCardReaderCollection(OcvSystemDependencyClass* systemMethods, const vector<wstring> filenamesOfImages, const bool doDebugging, const bool runParallelized) {
+
+	OcvCardCollectionReader *readers = new OcvCardCollectionReader(systemMethods, doDebugging, runParallelized);
+
+	//Add the cards to the reader collection
+	for (size_t i = 0; i < filenamesOfImages.size(); i++) {
+
+		wstring filename = filenamesOfImages[i];
+		readers->AddCard(filename);
+	}
+
+	return readers;
+}
+
+void OcvCardReaderApplication::reziseCommandWindow(OcvSystemDependencyClass* systemMethods, size_t numberOfFiles, int longestFilenameLength) {
+
+	int lettersToAccommodate = to_string(numberOfFiles).length() * 2 + longestFilenameLength + 75;
+	systemMethods->SetConsoleWidthInCharacters(lettersToAccommodate);
+}
+
+bool OcvCardReaderApplication::checkIfExecutionWasSuccessful(vector<OcvCardCollectionReader::CardNameInfo> executionResult) {
+
+	for (auto info : executionResult) {
+
+		if (!info.Success) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void OcvCardReaderApplication::printResultMessage(bool runWasSuccessful, int numberOfErrors) {
+
+	wstring message;
+	if (runWasSuccessful) {
+		message = L"All images was successfully read! Yeay! :-)";
+	}
+	else {
+		wstring amountOfErrorsStr = to_wstring(numberOfErrors);
+		wstring errorWord = (numberOfErrors == 1) ? L"error" : L"errors";
+		message = L"Oh no! There was " + amountOfErrorsStr + L" " + errorWord + L" when reading the cards! :-(";
+	}
+	wcout << endl << message << endl;
+}
+
+void OcvCardReaderApplication::printToManyFilesMessage() {
+
+	wcout
+		<< endl
+		<< L"Glunck! There were to many cards for me to handle! Don't feed me more than "
+		+ to_wstring(OcvCardCollectionReader::MaxSize())
+		+ L" card image files!"
+		<< endl;
+}
+
+long long OcvCardReaderApplication::getexecutionTime(TimePoint startTime, TimePoint endTime) {
+	
+	return chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
+}
+
+void OcvCardReaderApplication::printExecutionTimeMessage(TimePoint startTime, TimePoint endTime, int numberOfFilesExecuted, bool showTimeInSeconds) {
+
+	auto executionDurationTime = getexecutionTime(startTime, endTime);
+	wstring exeTime = showTimeInSeconds ? to_wstring(executionDurationTime / (float)1000000) : to_wstring(executionDurationTime);
+	wstring timeUnit = showTimeInSeconds ? L"seconds" : L"microseconds";
+	wcout << endl << L"The reading took " + exeTime + L" " + timeUnit + L" to execute." << endl;
+	wcout << L"That's " + to_wstring(executionDurationTime / (numberOfFilesExecuted * (float)1000000)) + L" seconds per card on average!" << endl << endl;
 }
--- ReadMagicCard/OcvCardReaderApplication.h
+++ ReadMagicCard/OcvCardReaderApplication.h
@@ -1,11 +1,38 @@
 #pragma once
+#include "OcvSystemDependencyClass.h"
+#include "OcvCardCollectionReader.h"
+#include <chrono>
 class OcvCardReaderApplication
 {
 public:
+	//A chrono time point for steady clock.
+	typedef std::chrono::time_point<std::chrono::steady_clock> TimePoint;
+
 	OcvCardReaderApplication();
 	~OcvCardReaderApplication();
 
 	//Runs the card reader application.
 	int Run(const bool doDebugging, const bool runParallelized);
+
+private:
+
+	//Removes lingering old data from previous runs.
+	void removeOldData(OcvSystemDependencyClass* systemMethods);
+	//Fetches the file names of the MtG image files.
+	std::vector<std::wstring> getMtgImageFileNames(OcvSystemDependencyClass* systemMethods);
+	//Creates an collection holding a reader for evere card image.
+	OcvCardCollectionReader* createCardReaderCollection(OcvSystemDependencyClass* systemMethods, const std::vector<std::wstring> filenamesOfImages, const bool doDebugging, const bool runParallelized);
+	//Resizes the command window to occupy as little space as possible without creating line breakes in its' output texts. 
+	void reziseCommandWindow(OcvSystemDependencyClass* systemMethods, size_t numberOfFiles, int longestFilenameLength);
+	//Checks if the execution was successful, aka without errors.
+	bool checkIfExecutionWasSuccessful(std::vector<OcvCardCollectionReader::CardNameInfo> executionResult);
+	//Prints a message telling the user how the execution went.
+	void printResultMessage(bool runWasSuccessful, int numberOfErrors);
+	//Prints a message telling the user that there are to many files to successfully read them all.
+	void printToManyFilesMessage();
+	//Extracts the time (in milliseconds) that passed between two time points.
+	long long getexecutionTime(TimePoint startTime, TimePoint endTime);
+	//Prints a message telling the user the length of the execution time.
+	void printExecutionTimeMessage(TimePoint startTime, TimePoint endTime, int numberOfFilesExecuted, bool showTimeInSeconds);
 };
 
