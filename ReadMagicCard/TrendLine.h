edc5c16cd85754ae59c216c6cc6f6bea33c9664d
Björn Larsson <registrering+github@bjornlarsson.se>
2018-04-19 15:41:52 +0200

    Comments.

--- ReadMagicCard/TrendLine.h
+++ ReadMagicCard/TrendLine.h
@@ -6,12 +6,13 @@ public:
 	TrendLine(double slope, double offset);
 	~TrendLine();
 
+	//Gets the end points of the line.
 	std::vector<cv::Point2f> GetEndPoints(float leftLimitX, float rightLimitX);
-
+	//Gets the Y value for a given X value.
 	double GetY(double x);
-
+	//Gets the X value for a given Y value.
 	double GetX(double y);
-
+	//Tells if the line is a zero line, i.e equivalent with most often is the X-axis.
 	bool IsZeroLine();
 
 	//The slope of the line.
