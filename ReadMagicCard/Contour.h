11c9d0e5b22baa39a87733badfbaf8ebc5409c67
Björn Larsson <registrering+github@bjornlarsson.se>
2018-04-27 10:43:08 +0200

    Added contour type made of doubles.

--- ReadMagicCard/Contour.h
+++ ReadMagicCard/Contour.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <opencv2\core.hpp>
-//Type used for specifying a contour.
+//Type used for specifying a contour (using int).
 typedef std::vector<cv::Point> Contour;
-//Type used as a collection of contours.
+//Type used for specifying a contour (using double).
+typedef std::vector<cv::Point2d> DblContour;
+//Type used as a collection of contours (using int).
 typedef std::vector<Contour> Contours;
+//Type used as a collection of contours (using double).
+typedef std::vector<DblContour> DblContours;
