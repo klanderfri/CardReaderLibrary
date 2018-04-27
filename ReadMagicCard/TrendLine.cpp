d8912ab9df0013e4c938ef62b443fb9a50dd842c
Björn Larsson <registrering+github@bjornlarsson.se>
2018-04-27 10:23:14 +0200

    Changed to get a little more precision and added methods.

--- ReadMagicCard/TrendLine.cpp
+++ ReadMagicCard/TrendLine.cpp
@@ -2,20 +2,23 @@
 #include "TrendLine.h"
 #include "AlgorithmHelper.h"
 #include "SystemMethods.h"
+#include "math.h"
 
 using namespace cv;
 using namespace std;
+
+TrendLine TrendLine::XAxis = TrendLine(0, 0);
 
 TrendLine::TrendLine()
 {
 }
 
-TrendLine::TrendLine(double slope, double offset)
+TrendLine::TrendLine(long double slope, long double offset)
 {
 	setupMemberVariables(slope, offset);
 }
 
-TrendLine::TrendLine(const vector<Point>& points) {
+TrendLine::TrendLine(const vector<Point2d>& points) {
 
 	if (points.empty()) {
 
@@ -24,14 +27,14 @@ TrendLine::TrendLine(const vector<Point>& points) {
 	else if (points.size() == 1) {
 
 		Point2d origo(0, 0);
-		vector<Point> newVector{ origo, points[0] };
+		vector<Point2d> newVector{ origo, points[0] };
 		TrendLine line(newVector);
 
 		setupMemberVariables(line.Slope, line.Offset);
 	}
 	else {
 
-		vector<double> x, y;
+		vector<long double> x, y;
 
 		for (Point2d point : points) {
 
@@ -39,9 +42,9 @@ TrendLine::TrendLine(const vector<Point>& points) {
 			y.push_back(point.y);
 		}
 
-		double slope = AlgorithmHelper::Slope(x, y);
-		Point2d point((float)AlgorithmHelper::Average(x), (float)AlgorithmHelper::Average(y));
-		double offset = point.y - (slope * point.x);
+		long double slope = AlgorithmHelper::Slope(x, y);
+		Point2d point(AlgorithmHelper::Average(x), AlgorithmHelper::Average(y));
+		long double offset = point.y - (slope * point.x);
 
 		setupMemberVariables(slope, offset);
 	}
@@ -51,16 +54,16 @@ TrendLine::~TrendLine()
 {
 }
 
-void TrendLine::setupMemberVariables(double slope, double offset) {
+void TrendLine::setupMemberVariables(long double slope, long double offset) {
 
 	this->Slope = slope;
 	this->Offset = offset;
 }
 
-vector<Point2d> TrendLine::GetEndPoints(float leftLimitX, float rightLimitX) {
+vector<Point2d> TrendLine::GetEndPoints(long double leftLimitX, long double rightLimitX) {
 
-	Point2d linePoint1(leftLimitX, (float)(Offset + Slope * leftLimitX));
-	Point2d linePoint2(rightLimitX, (float)(Offset + Slope * rightLimitX));
+	Point2d linePoint1(leftLimitX, Offset + Slope * leftLimitX);
+	Point2d linePoint2(rightLimitX, Offset + Slope * rightLimitX);
 
 	vector<Point2d> linePoints;
 	linePoints.push_back(linePoint1);
@@ -69,19 +72,19 @@ vector<Point2d> TrendLine::GetEndPoints(float leftLimitX, float rightLimitX) {
 	return linePoints;
 }
 
-double TrendLine::GetY(double x) {
+long double TrendLine::GetY(long double x) {
 
 	return Slope * x + Offset;
 }
 
-double TrendLine::GetX(double y) {
+long double TrendLine::GetX(long double y) {
 
 	return (y - Offset) / Slope;
 }
 
 bool TrendLine::IsZeroLine() {
 
-	return (Offset == 0 && Slope == 0);
+	return (Offset == XAxis.Offset && Slope == XAxis.Slope);
 }
 
 wstring TrendLine::GetEquation() {
@@ -105,7 +108,23 @@ wstring TrendLine::GetZeroEquation() {
 	return equation;
 }
 
-double TrendLine::GetPerpendicularDistance(Point point) {
+long double TrendLine::GetPerpendicularDistance(TrendLine lineA, TrendLine lineB) {
+
+	//Implemented according to:
+	//https://en.wikipedia.org/w/index.php?title=Distance_between_two_straight_lines&oldid=816643023
+
+	if (lineA.Slope != lineB.Slope) {
+		throw OperationException("The lines are not parallel and therefore the perpendicular distance cannot be calculated.");
+	}
+
+	long double numerator = abs(lineB.Offset - lineA.Offset);
+	long double denominator = sqrt(pow(lineA.Slope, 2) + 1);
+	long double distance = numerator / denominator;
+
+	return distance;
+}
+
+long double TrendLine::GetPerpendicularDistance(Point2d point) {
 
 	//Check if the line is parallel with the X axis.
 	//It's a special case that could be calculated quicker,
@@ -118,64 +137,77 @@ double TrendLine::GetPerpendicularDistance(Point point) {
 	//Find distance using intersection;
 	TrendLine perpendicularLine = GetPerpendicularLine(point);
 	Point2d intersection = GetIntersectionPoint(*this, perpendicularLine);
-	double controlDistance = AlgorithmHelper::FindDistance(point, intersection);
+	long double controlDistance = AlgorithmHelper::FindDistance(point, intersection);
 
 	//Implemented according to:
 	//https://www.slideshare.net/nsimmons/11-x1-t05-05-perpendicular-distance
 	//Also check "Line defined by an equation" at Wikipedia:
 	//https://en.wikipedia.org/w/index.php?title=Distance_from_a_point_to_a_line&oldid=837108058#Line_defined_by_an_equation
 
-	double a = (-1) * Slope;
-	double b = (1);
-	double c = (-1) * Offset;
+	long double a = (-1) * Slope;
+	long double b = (1);
+	long double c = (-1) * Offset;
 
-	double numerator = a * point.x + b * point.y + c;
-	double denominator = sqrt(pow(a, 2) + pow(b, 2)); //Will never be negative or zero.
+	long double numerator = a * point.x + b * point.y + c;
+	long double denominator = sqrt(pow(a, 2) + pow(b, 2)); //Will never be negative or zero.
 
-	double distance = numerator / denominator;
+	long double distance = numerator / denominator;
 
 	//The two methods should render the same result but considering the result being doubles
 	//after valculations with doubles, they most likely have small differences.
 	
 	//The first method can't determine which side the point is relative the line, hense absolute value method.
-	double difference = abs(controlDistance - abs(distance));
+	long double difference = abs(controlDistance - abs(distance));
 
 	//Although, too much of a difference indicates error in the algorithms.
-	if (difference >= 0.1) {
-		throw OperationException("The methods calculating the perpendicular distance renders different results!");
-	}
+	assert(difference < 0.1);
 
 	//Use the average value.
-	double sign = (distance < 0) ? -1 : 1;
-	double value = AlgorithmHelper::Average(vector<double> { controlDistance, abs(distance) });
+	long double sign = (distance < 0) ? -1 : 1;
+	long double value = AlgorithmHelper::Average(vector<long double> { controlDistance, abs(distance) });
 	distance = sign * value;
 
 	return distance;
 }
 
-TrendLine TrendLine::GetPerpendicularLine(Point pointOnPerpendicularLine) {
+TrendLine TrendLine::GetPerpendicularLine(Point2d pointOnPerpendicularLine) {
 
 	//Implemented according to section "Perpendicular Lines" at
+	//http://www.purplemath.com/modules/strtlneq3.htm
 	//https://www.mathsisfun.com/algebra/line-parallel-perpendicular.html
 
 	if (Slope == 0) {
-		throw OperationException("Perpendicular lines can't be calculated when the slope is zero!");
+		throw OperationException("Perpendicular lines cannot be calculated when the slope is zero!");
 	}
 
-	double slope = abs(1 / Slope);
-	double offset = slope * (-1) * pointOnPerpendicularLine.x + pointOnPerpendicularLine.y;
-
+	//Calculate perpendicular line.
+	long double slope = (-1 / Slope);
+	long double offset = slope * (-1) * pointOnPerpendicularLine.x + pointOnPerpendicularLine.y;
 	TrendLine perpendicularLine(slope, offset);
+
+	//Verify that the line is perpendicular.
+	long double lineAngles = TrendLine::GetAngleBetweenLines(*this, perpendicularLine);
+	//The perpendicular line is not perpendicular!
+	assert(abs(lineAngles) == 90);
 
 	return perpendicularLine;
 }
 
-TrendLine TrendLine::GetParallelLine(Point pointOnParallelLine) {
+TrendLine TrendLine::GetParallelLine(Point2d pointOnParallelLine) {
 
-	double y = GetY(pointOnParallelLine.x);
-	double extraOffset = pointOnParallelLine.y - y;
-
+	//Calculate parallel line.
+	long double xOnThisLine = pointOnParallelLine.x;
+	long double yOnThisLine = GetY(xOnThisLine);
+	long double extraOffset = pointOnParallelLine.y - yOnThisLine;
 	TrendLine parallel(Slope, Offset + extraOffset);
+
+	//Verify that the line is parallel.
+	long double yOnParallelLine = parallel.GetY(pointOnParallelLine.x);
+	long double difference = abs(yOnParallelLine - pointOnParallelLine.y);
+	//The methods calculating the parallel line got an inaccurate result!
+	assert(difference < FLT_EPSILON);
+	//The methods calculating the parallel line got a line that is not parallel!
+	assert(this->Slope == parallel.Slope);
 
 	return parallel;
 }
@@ -189,42 +221,78 @@ Point2d TrendLine::GetIntersectionPoint(TrendLine lineA, TrendLine lineB) {
 		throw OperationException("The slopes are identical, meaning the lines are parallel and has no intersection point!");
 	}
 
-	double x = (lineB.Offset - lineA.Offset) / (lineA.Slope - lineB.Slope); //Will never be division by zero due to the exception above.
-	double y = lineA.GetY(x);
+	long double x = (lineB.Offset - lineA.Offset) / (lineA.Slope - lineB.Slope); //Will never be division by zero due to the exception above.
+	long double y = lineA.GetY(x);
 
 	Point2d intersection(x, y);
 
 	return intersection;
 }
 
-vector<TrendLine> TrendLine::GetBoundLines(vector<Point> points) {
+long double TrendLine::GetAngleBetweenLines(TrendLine normLine, TrendLine relationLine) {
 
-	if (points.size() < 3) {
-		return vector<TrendLine> { this, this };
+	//Implemented according to:
+	//https://www.youtube.com/watch?v=JSEPDJfl8m8
+
+	long double numerator = normLine.Slope - relationLine.Slope;
+	long double denominator = 1 + normLine.Slope * relationLine.Slope;
+	long double value = numerator / denominator;
+	long double angle = atan(value) * 180 / CV_PI;
+
+	return angle;
+}
+
+long double TrendLine::GetAngleToAxisX() {
+
+	long double angle = GetAngleBetweenLines(XAxis, *this);
+	return angle;
+}
+
+vector<TrendLine> TrendLine::GetBoundLines(vector<Point2d> points) {
+
+	if (points.size() == 0) {
+		throw ParameterException("There was no points to bound!", "points");
 	}
 
-	Point lowestPoint = points[0], highestPoint = points[0];
-	double lowestPointDistance = GetPerpendicularDistance(lowestPoint);
-	double highestPointDistance = lowestPointDistance;
+	vector<TrendLine> bounds;
 
-	for (size_t i = 1; i < points.size(); i++) {
+	if (points.size() == 1) {
+		
+		TrendLine line = GetParallelLine(points[0]);
+		bounds.push_back(line);
+		bounds.push_back(line);
+	}
+	else {
 
-		Point point = points[i];
-		double distance = GetPerpendicularDistance(point);
+		Point2d lowestPoint = points[0], highestPoint = points[0];
+		long double lowestPointDistance = GetPerpendicularDistance(lowestPoint);
+		long double highestPointDistance = lowestPointDistance;
 
-		if (distance < lowestPointDistance) {
-			lowestPointDistance = distance;
-			lowestPoint = point;
+		for (size_t i = 1; i < points.size(); i++) {
+
+			Point2d point = points[i];
+			long double distance = GetPerpendicularDistance(point);
+
+			if (distance < lowestPointDistance) {
+				lowestPointDistance = distance;
+				lowestPoint = point;
+			}
+			if (distance > highestPointDistance) {
+				highestPointDistance = distance;
+				highestPoint = point;
+			}
 		}
-		if (distance > highestPointDistance) {
-			highestPointDistance = distance;
-			highestPoint = point;
-		}
+
+		TrendLine lowestBound = GetParallelLine(lowestPoint);
+		TrendLine highestBound = GetParallelLine(highestPoint);
+
+		bounds.push_back(lowestBound);
+		bounds.push_back(highestBound);
 	}
 
-	TrendLine lowestBound = GetParallelLine(lowestPoint);
-	TrendLine highestBound = GetParallelLine(highestPoint);
+	//The bounding lines must be parallel!
+	assert(this->Slope == bounds[0].Slope);
+	assert(bounds[0].Slope == bounds[1].Slope);
 
-	vector<TrendLine> bounds{ lowestBound, highestBound };
 	return bounds;
 }
--- ReadMagicCard/TrendLine.h
+++ ReadMagicCard/TrendLine.h
@@ -3,41 +3,50 @@ class TrendLine
 {
 public:
 	TrendLine();
-	TrendLine(double slope, double offset);
-	TrendLine(const std::vector<cv::Point>& points);
+	TrendLine(long double slope, long double offset);
+	TrendLine(const std::vector<cv::Point2d>& points);
 	~TrendLine();
 
 	//Gets the end points of the line.
-	std::vector<cv::Point2d> GetEndPoints(float leftLimitX, float rightLimitX);
+	std::vector<cv::Point2d> GetEndPoints(long double leftLimitX, long double rightLimitX);
 	//Gets the Y value for a given X value.
-	double GetY(double x);
+	long double GetY(long double x);
 	//Gets the X value for a given Y value.
-	double GetX(double y);
+	long double GetX(long double y);
 	//Tells if the line is a zero line, i.e equivalent with most often is the X-axis.
 	bool IsZeroLine();
 	//Gets the equation in the form 'y = kx + m'.
 	std::wstring GetEquation();
 	//Gets the equation in the form 'y - kx - m = 0'.
 	std::wstring GetZeroEquation();
+	//Gets the perpendicular distance between two lines.
+	static long double GetPerpendicularDistance(TrendLine lineA, TrendLine lineB);
 	//Gets the perpendicular distance between the line and a point.
-	double GetPerpendicularDistance(cv::Point point);
+	long double GetPerpendicularDistance(cv::Point2d point);
 	//Gets another line perpendicular to the line.
-	TrendLine GetPerpendicularLine(cv::Point pointOnPerpendicularLine);
+	TrendLine GetPerpendicularLine(cv::Point2d pointOnPerpendicularLine);
 	//Gets another line parallel to the line.
-	TrendLine GetParallelLine(cv::Point pointOnParallelLine);
+	TrendLine GetParallelLine(cv::Point2d pointOnParallelLine);
 	//Gets the point in which thw two lines meet.
 	static cv::Point2d GetIntersectionPoint(TrendLine lineA, TrendLine lineB);
+	//Gets the clockwise angle from the norm line to the relation line.
+	static long double GetAngleBetweenLines(TrendLine normLine, TrendLine relationLine);
+	//Gets the angle between the line and the X axis.
+	long double GetAngleToAxisX();
 	//Gets the two lines parallel to the line, having all the points between them.
-	std::vector<TrendLine> GetBoundLines(std::vector<cv::Point> points);
+	std::vector<TrendLine> GetBoundLines(std::vector<cv::Point2d> points);
 
 	//The slope of the line.
-	double Slope = 0;
+	long double Slope = 0;
 	//The offset of the line.
-	double Offset = 0;
+	long double Offset = 0;
+
+	//The line representing the X axis.
+	static TrendLine XAxis;
 
 private:
 
 	//Sets up the member variables.
-	void setupMemberVariables(double slope, double offset);
+	void setupMemberVariables(long double slope, long double offset);
 };
 
