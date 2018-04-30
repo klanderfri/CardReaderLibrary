cddc10eefd14ede12a271c5ab6991f2da550cc24
Björn Larsson <registrering+github@bjornlarsson.se>
2018-04-30 19:38:03 +0200

    Created methods helping handling of letter areas.

--- ReadMagicCard/LetterArea.cpp
+++ ReadMagicCard/LetterArea.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "LetterArea.h"
 
+using namespace cv;
+using namespace std;
+
 LetterArea::LetterArea()
 {
 }
@@ -9,12 +12,17 @@ LetterArea::~LetterArea()
 {
 }
 
-bool LetterArea::CompareLetterAreaByLeftBorderXAscending(LetterArea area1, LetterArea area2)
-{
+bool LetterArea::CompareLetterAreaByLeftBorderXAscending(LetterArea area1, LetterArea area2) {
+
 	float borderX1 = area1.Box.center.x - ImageHelper::SmallestDistanceCenterToLimit(area1.Box);
 	float borderX2 = area2.Box.center.x - ImageHelper::SmallestDistanceCenterToLimit(area2.Box);
 
 	return (borderX1 < borderX2);
+}
+
+bool LetterArea::CompareLetterAreaByHeightAscending(LetterArea area1, LetterArea area2) {
+
+	return (area1.Box.size.height < area2.Box.size.height);
 }
 
 bool LetterArea::HasParentLetter(LetterArea letterToCheck, LetterAreas possibleParents) {
@@ -40,4 +48,29 @@ bool LetterArea::hasChildParentRelation(LetterArea child, LetterArea parent) {
 
 	//Close enough to be a child.
 	return true;
+}
+
+Point2d LetterArea::GetMiddleBottomPoint() {
+
+	//Implemented according to section "But which one?" at:
+	//http://www.mathsisfun.com/algebra/trig-inverse-sin-cos-tan.html
+
+	double height = Box.size.height;
+	double angle = Box.angle;
+	if (angle < -45) {
+
+		height = Box.size.width;
+		angle += 90.0;
+	}
+
+	double hypotenuse = height / 2;
+	double angleFactor = (-1) * sin(angle * CV_PI / 180);
+	double xdistance = angleFactor * hypotenuse;
+	double yDistance = sqrt(pow(hypotenuse, 2) - pow(xdistance, 2));
+
+	double x = Box.center.x + xdistance;
+	double y = Box.center.y + yDistance;
+	Point2d bottomMiddle(x, y);
+
+	return bottomMiddle;
 }
--- ReadMagicCard/LetterArea.h
+++ ReadMagicCard/LetterArea.h
@@ -11,8 +11,12 @@ public:
 
 	//Compares two letter areas by the X-coordinate of the left border when the letters are standing stright up.
 	static bool CompareLetterAreaByLeftBorderXAscending(LetterArea area1, LetterArea area2);
+	//Compares two letter areas by the height of the bounding rectangle.
+	static bool CompareLetterAreaByHeightAscending(LetterArea area1, LetterArea area2);
 	//Checks if a letter is a child to any other letter.
 	static bool HasParentLetter(LetterArea letterToCheck, LetterAreas possibleParents);
+	//Gets the point marking the middle of the bottom line of the letter box.
+	cv::Point2d GetMiddleBottomPoint();
 
 	//The outer contour which wraps the outer limits of the letter.
 	Contour OuterContour;
