f0575315630cbe0f067d41f620f6f414a4fbc6ac
klanderfri <registrering+github@bjornlarsson.se>
2018-05-25 18:51:50 +0200

    Comments.

--- ReadMagicCard/DrawingHelper.h
+++ ReadMagicCard/DrawingHelper.h
@@ -15,21 +15,21 @@ public:
 	//The line type (8-connected line, aka "solid") of borders drawn on images for debugging.
 	static const int DEFAULT_BORDER_LINE_TYPE;
 
-	//Draws a line on an image between two points.
+	//Draws a line between two points on an image.
 	cv::Mat DrawLine(const cv::Mat image, cv::Point point1, cv::Point point2, Colour colour = Purple, int borderThickness = DEFAULT_BORDER_THICKNESS);
 	//Draws a line on an image.
 	cv::Mat DrawLine(const cv::Mat image, TrendLine line, Colour colour = Purple, int borderThickness = DEFAULT_BORDER_THICKNESS);
-	//Draws the specified limits to the image.
+	//Draws the specified limits on an image.
 	cv::Mat DrawLimits(const cv::Mat image, const Contour contourLimit = Contour(), const cv::RotatedRect rotatedRectangleLimit = cv::RotatedRect(), const cv::Rect straightRectangleLimit = cv::Rect());
-	
+	//Draws a rectangle on an image.
 	cv::Mat DrawRectangle(const cv::Mat image, const cv::RotatedRect rectangle, Colour colour = DarkBlue);
-
+	//Draws a rectangle on an image.
 	cv::Mat DrawRectangle(const cv::Mat image, const cv::Rect rectangle, Colour colour = Green);
-	//Draws the specified limits to the image.
+	//Draws the specified contours on an image.
 	cv::Mat DrawContours(const cv::Mat image, const Contours contours, bool useRandomColours = false, const Hierarchy hierarchy = Hierarchy());
-	//Creates an image showing the limits of the individual letters.
+	//Draws the specified letter areas on an image.
 	cv::Mat DrawLetterAreas(const cv::Mat image, const LetterAreas letters, int letterCenterRadius);
-	//Draws the specified center point to the image.
+	//Draws a circle on an image.
 	cv::Mat DrawCircle(const cv::Mat image, const cv::Point imageCenter, int radius = 3, const Colour colour = Red);
 	//Fills a contour in the image with the specified colour.
 	void FillContour(cv::Mat& image, const Contour contour, const Colour colour);
